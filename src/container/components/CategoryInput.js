ff250087097b7175ec71a2fe90b1bda80253ff3c
FarrelHI <farrel.indallah@gmail.com>
2023-09-16 08:57:03 +0700

    update css

--- src/container/components/CategoryInput.js
+++ src/container/components/CategoryInput.js
@@ -1,11 +1,51 @@
+import React, {useState} from "react";
+
 import "./CategoryInput.css"
 
 function CategoryInput(props){
+    const [value, setValue] = useState('');
+    const [pickColor, setPickColor] = useState('#FFFFFF');
+
+    function checkValidDate(dateValue) {
+        if(props.date === 'DAY'){
+            if(dateValue < 1 && dateValue>31){
+                setPickColor("#FF0000");
+            }
+        }
+        else if(props.date === 'MONTH'){
+            if(dateValue < 1 && dateValue>12){
+                setPickColor("#FF0000");
+            }
+        }
+        else if(props.date === 'YEAR'){
+            if(dateValue < 1900 && dateValue>2023){
+                setPickColor("#FF0000");
+            }
+        }
+    }
+
+    function checkInputType(checkValue) {
+        if(parseInt(checkValue)*1 !== parseInt(checkValue)){
+            setPickColor("#FF0000");
+        }
+        else{
+            setPickColor("#FFFFFF");
+            checkValidDate(checkValue);
+        }
+    }
+
+    const changeHandler = event =>{
+        let newValue = event.target.value;
+        setValue(newValue);
+
+        checkInputType(newValue);
+    }
+
     return(
         <div className="input-container">
             <p>{props.date}</p>
-            <input type='text' className="input-field" placeholder={props.placeholder}></input>
-            <p id="warning-desc">{props.warning}</p>
+            <input type='text' className="input-field" placeholder={props.placeholder} value={value} onChange={changeHandler}></input>
+            <p id="warning-desc" style={{color: pickColor}}>{props.warning}</p>
         </div>
     )
 }
